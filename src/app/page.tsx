15a52d3d4d3b5288963f1b6b296e370e47e54ce4
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2024-07-05 22:06:16 -0400

    Added mobile and tab view, desktop view needs changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,12 @@ import { HomeHandlers } from "./utils/handlers";
 
 export default function Home() {
   const [text] = useTypewriter({
-    words: ["Goutham Ananda", "an Aerospace Stress Engineer"],
+    words: ["Goutham Ananda", "an Aerospace Stress Analyst"],
     typeSpeed: 100,
+    loop: true,
   });
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-small-image lg:bg-large-image md:bg-medium-image bg-cover bg-center text-white">
+    <div className="flex flex-col items-center justify-center  bg-small-image lg:bg-large-image md:bg-medium-image bg-cover bg-center text-white h-screen overflow-hidden">
       <Head>
         <title>Goutham Ananda | Portfolio</title>
         <meta
@@ -21,25 +22,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="relative container md:px-40 md:py-32 h-full">
+      <main className="lg:relative container h-full py-5 px-5 md:py-20 md:px-10">
         <div className="absolute inset-0 bg-black opacity-60"></div>
-        <h1 className="relative text-3xl text-start self-start w-full z-10">
+        <h1 className="relative text-3xl md:text-5xl text-start self-start w-full z-10">
           G-A
         </h1>
-        <div className="relative h-full grid grid-cols-1 md:grid-cols-1 lg:grid-cols-[60%_40%] justify-center items-center z-10">
-          <div className="h-full flex flex-col justify-evenly order-2 lg:order-1">
-            <h2 className="text-6xl font-semibold text-cyan-400">Hello!</h2>
-            <p className="text-3xl font-normal">
+        <div className="relative h-full grid grid-cols-1 grid-rows-[40%_60%] md:grid-cols-1 lg:grid-cols-[60%_40%] justify-center items-center z-10">
+          <div className="h-full flex flex-col gap-6 order-2 lg:order-1">
+            <h2 className="text-4xl md:text-6xl font-semibold text-cyan-400">
+              Hello!
+            </h2>
+            <p className="text-lg md:text-4xl font-normal">
               I am <span className="font-semibold">{text}</span>
               <Cursor />
             </p>
 
-            <p className="text-xl">
+            <p className="text-sm md:text-2xl">
               Welcome to my page ! This is a work in progress and will be
-              updating shortly
+              updating shortly. Until then
             </p>
-            <p className="text-lg uppercase font-thin">Connect On</p>
-            <div className="text-3xl flex gap-5">
+            <p className="text-lg md:text-2xl uppercase font-thin">
+              Get in touch with
+            </p>
+            <div className="text-3xl md:text-4xl flex gap-5">
               <LinkedInIcon
                 className="cursor-pointer text-white"
                 onClick={HomeHandlers.handleRedirect(
@@ -60,7 +65,7 @@ export default function Home() {
             <div className="relative">
               <div className="absolute inset-0 rounded-full bg-cyan-700 bg-opacity-25"></div>
               <img
-                className="w-60 h-60 rounded-full border-4 border-cyan-400"
+                className="w-32 h-32 md:w-44 md:h-44 rounded-full border-4 border-cyan-400"
                 src="/assets/img/profile_pic.jpeg"
                 alt="Rounded avatar"
               />
