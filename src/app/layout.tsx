ab4584768ccfc09f2ecdbb8cb46a29fcdaa0d31f
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2024-07-21 22:16:19 -0400

    Updated title of the webapp and the icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Goutham Ananda - Portfolio",
+  title: "Aerospace Stress",
   description: "Goutham Ananda's Portfolio Website",
   icons: {
     icon: [
